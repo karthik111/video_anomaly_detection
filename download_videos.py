c0dd76e9a18474030b6a556163db40b3ce9ff7df
karthik.venkat <karthikeyanv@gmail.com>
2023-07-08 20:32:45 -0400

    Initial version

--- download_videos.py
+++ download_videos.py
@@ -15,10 +15,18 @@ def get_file_name(string):
 url = "https://www.dropbox.com/s/example/filename.zip?dl=1"
 # Replace the "example" and "filename.zip" with your actual Dropbox folder path and file name
 
+# change dl to 1 when copying urls below.
+
 url = "https://www.dropbox.com/sh/75v5ehq4cdg5g5g/AABJtkTnNc8LcVTfH1gE_uFoa/Anomaly-Videos-Part-2.zip?dl=1"
 url = "https://www.dropbox.com/sh/75v5ehq4cdg5g5g/AAAbdSEUox64ZLgVAntr2WgSa/Anomaly-Videos-Part-2.zip?dl=1"
 url ="https://www.dropbox.com/sh/75v5ehq4cdg5g5g/AADEUCsLOCN_jHmmx7uFcUhHa/Training-Normal-Videos-Part-1.zip?dl=1"
 url = "https://www.dropbox.com/sh/75v5ehq4cdg5g5g/AAD5w76F_SZLdBgxVdvko-z5a/UCF_Crimes-Train-Test-Split.zip?dl=1"
+url = "https://www.dropbox.com/sh/75v5ehq4cdg5g5g/AACeDPUxpB6sY2jKgLGzaEdra/Testing_Normal_Videos.zip?dl=1"
+url = "https://www.dropbox.com/sh/75v5ehq4cdg5g5g/AAAgpsRNSHI_BtRnSCxxR7j9a/Anomaly-Videos-Part-3.zip?dl=1"
+url = "https://www.dropbox.com/sh/75v5ehq4cdg5g5g/AABqY-3fJSmSMafFIlJXRE-9a/Anomaly-Videos-Part-4.zip?dl=1"
+url = "https://www.dropbox.com/sh/75v5ehq4cdg5g5g/AACyDI-0oRqqiqUcAulw_x5wa/Normal_Videos_for_Event_Recognition.zip?dl=1"
+url = "https://www.dropbox.com/sh/75v5ehq4cdg5g5g/AAAHZByMMGCVms4hhHZU2pMBa/Training-Normal-Videos-Part-2.zip?dl=1"
+url = 'https://www.dropbox.com/sh/75v5ehq4cdg5g5g/AADjSOQ-NLIsCVNWT0Mrhp5ca/Temporal_Anomaly_Annotation_for_Testing_Videos.txt?dl=1'
 
 file_name = get_file_name(url)
 
